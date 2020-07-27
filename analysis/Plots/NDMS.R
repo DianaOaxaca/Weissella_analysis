b17ab922a6e23a51cdd80800a9a8beaec523e5e3
obiwanlopez1986 <31995729+obiwanlopez1986@users.noreply.github.com>
2020-07-27 12:22:07 -0500

    Update NDMS.R

--- analysis/Plots/NDMS.R
+++ analysis/Plots/NDMS.R
@@ -1,24 +1,33 @@
 #/usr/local/bin/R
 #Rafael Lopez Sanchez
 
-### NMDS with Bray-Curtis dissimilitary index
-#1. Upload Mass and Vegan packages from R
-library("MASS")
-library("vegan")
+##Normalized OTU table.
 
-#2.otu_df<-read.table("normal_otu_table.txt", header=TRUE, row.names=1, sep="\t")
+#1. Upload Mass metagenomeSeq and Vegan packages from R
+library("metagenomeSeq")
+library("vegan")
+library("MASS")
+mydata<-load_meta("filtered_otu_table.txt")
+obj = newMRexperiment(mydata$counts)
+p = cumNormStatFast(obj)
+normalized_matrix = cumNormMat(obj, p = p)
+exportMat(normalized_matrix, file ="normal_otu_table.txt")
+
+##. NMDS with Bray-Curtis dissimilitary index
+#1. Read table, do matrix and transpond matrix.
+otu_df<-read.table("normal_otu_table.txt", header=TRUE, row.names=1, sep="\t")
 otu_mat<-as.matrix(otu_df)
 transp_otu_mat<-t(otu_mat)
 
-#3.Assign NDMS parameters using the Bray-Curtis index as default.
+#2.Assign NDMS parameters using the Bray-Curtis index as default.
 cru.mds_meta <- metaMDS(transp_otu_mat, trace = FALSE, k=2, trymax=100)
 var_stress<-round(cru.mds_meta$stress, 6)
 
-#4.Assign color pallette.
+#3.Assign color pallette.
 my_color=c(rep("#FF00FF", 4), rep("#04CA4C", 4), rep("#2177CC", 4))
 png("metamds_bray.png", width = 5*300, height = 5*300, res = 400, pointsize = 8)
 
-#5.Make the NDMS plot. 
+#4.Make the NDMS plot. 
 plot(cru.mds_meta, type = "n")
 text(cru.mds_meta, labels = row.names(transp_otu_mat), cex=0.7, col=my_color)
 coord<-par("usr")
