167a3aed4e28f3a72d0beb82449f70c788ad8361
obiwanlopez1986 <31995729+obiwanlopez1986@users.noreply.github.com>
2020-07-27 09:14:55 -0500

    Update Taxonomy_16S.sh

--- analysis/Taxonomy_16S/Taxonomy_16S.sh
+++ analysis/Taxonomy_16S/Taxonomy_16S.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/bash
-#Workflow to 16S Taxonomy analysis by Rafael LÃpez SÃ¡nhez
+#Workflow to 16S Taxonomy analysis by Rafael Lopez Sanchez
+
+#Check base quality with the basic_stats.pl script made from Dr. Grisel Alejandra Escobar Zepeda 
+basic_stats.pl files_list.txt
 
