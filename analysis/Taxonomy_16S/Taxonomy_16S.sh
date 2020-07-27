c69957b1af59ca7b7fb997195804c0fa0c9a023e
obiwanlopez1986 <31995729+obiwanlopez1986@users.noreply.github.com>
2020-07-27 09:36:49 -0500

    Update Taxonomy_16S.sh

--- analysis/Taxonomy_16S/Taxonomy_16S.sh
+++ analysis/Taxonomy_16S/Taxonomy_16S.sh
@@ -1,23 +1,16 @@
 #!/usr/bin/bash
-#Workflow to 16S taxonomy analysis by Rafael Lopez Sanchez.Pipeline used
+#Workflow to 16S taxonomy analysis by Rafael Lopez Sanchez. Pipeline used
 
-#We use the pipeline of Parallel-Meta v.2.4.1 
+#1. pipeline of Parallel-Meta v.2.4.1 
 parallel-meta -b B -r flash_out.extendedFrags.fasta -d X -n 4
 
-## Transform the taxonomy.txt file into an abundance matrix. We use the taxonomy2lineage.sh from Dr Grisel Alejandra Escobar Zepeda.
-$taxonomy2lineage.sh 
+#2.Transform the taxonomy.txt file into an abundance matrix with the name taxonomy2lineage.txt. We use the taxonomy2lineage.sh from Dr Grisel Alejandra Escobar Zepeda.
+./taxonomy2lineage.sh 
 
-## Integramos todas las matrices en una sola. El script que lo hace es /home/ales/scripts/matrix_integrator_bmk.pl
-## Vamos a ponerla en una carpeta aparte
-$ mkdir integrated_matrix && cd integrated_matrix
+#.3 Integrate the taxonomy2lineage.txt files into a matrix using the matrix_integrator_bmk.pl script written by Dr Grisel Alejandra Escobar Zepeda and a list file of the folders called name.file.
+matrix_integrator_bmk.pl names.file
 
-## Hacemos ligas simbolicas de todos los resultados y corremos el programa
-$ mv ../names.file .
-$ for cosa in $(cat names.file); do (ln -s ../$cosa/Result/taxonomy2lineage.txt $cosa); done
-$ /home/ales/scripts/matrix_integrator_bmk.pl names.file
-$ less integrated_matrix.txt
-
-## ¿Tenemos anotacion para dominios que no sean Bacteria o Archaea? ¿Cuantos son? Nos conviene eliminarlos
+#4. Eliminate Eukaryota, and Chloroplast sequences from the integrated matrix.
 $ cut -d ';' -f1 integrated_matrix.txt | sort | uniq
 $ grep -c '^Chloroplast' integrated_matrix.txt
 $ grep -c '^Eukaryota' integrated_matrix.txt
