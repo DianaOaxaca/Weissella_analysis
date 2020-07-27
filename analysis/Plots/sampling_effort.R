794e318c3d64114710e3d724df7be1dbd9dcb4f6
obiwanlopez1986 <31995729+obiwanlopez1986@users.noreply.github.com>
2020-07-27 10:23:35 -0500

    Update sampling_effort.R

--- analysis/Plots/sampling_effort.R
+++ analysis/Plots/sampling_effort.R
@@ -6,24 +6,28 @@ library("phyloseq")
 library("metagenomeSeq")
 library("vegan")
 library("MASS")
+
 #2.Making the matrix from the filtered_otu_table.txt file
 otu_df<-read.table("filtered_otu_table.txt", header=TRUE, row.names=1, sep="\t")
 otu_mat<-as.matrix(otu_df)
+
 #3.Making the transposed matrix.
 transp_otu_mat<-t(otu_mat)
+
 #4.Choosing the colors for variables.
 mi_color=c(“yellow”, "deepskyblue", "deeppink", “green”, “red”,”orange”)
+
+#5.Rarefaction curves.
 S <- specnumber(transp_otu_mat)
 raremax <- min(rowSums(transp_otu_mat))
 otu<-rarecurve(transp_otu_mat, step = 200, sample = raremax, col=mi_color, cex = 0.6)
 Nmax <- sapply(out, function(x) max(attr(x, "Subsample")))
 Smax <- sapply(out, max)
+
+#6.Plotting a png image.
 png("rarefaction_locator.png", width = 7*300, height = 5*300, res = 400, pointsize = 8)
-
 plot(c(1, max(Nmax)), c(1, max(Smax)), xlab = "Sample Size", ylab = "Species", type = "n", cex.lab=0.8, cex.axis=0.8)
-
 #abline(v = raremax)
-
 for (i in seq_along(out)) {
 N <- attr(out[[i]], "Subsample")lines(N, out[[i]], col = mi_color[i])text(locator(1), labels=row.names(transp_otu_mat)[i], cex=0.8)
 }
