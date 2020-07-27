7e0be9154a956b2689844aa1fc4aeae07c2a8a80
obiwanlopez1986 <31995729+obiwanlopez1986@users.noreply.github.com>
2020-07-27 10:08:30 -0500

    Update sampling_effort.R

--- analysis/Plots/sampling_effort.R
+++ analysis/Plots/sampling_effort.R
@@ -1,11 +1,17 @@
+#/usr/local/bin/R
+#Rafael Lopez Sanchez
 
+#1.Upload the R packages phyloseq, metagenomeSeq, vegan and MASS
 library("phyloseq")
 library("metagenomeSeq")
 library("vegan")
 library("MASS")
+#2.Making the matrix from the filtered_otu_table.txt file
 otu_df<-read.table("filtered_otu_table.txt", header=TRUE, row.names=1, sep="\t")
 otu_mat<-as.matrix(otu_df)
+#3.Making the transposed matrix.
 transp_otu_mat<-t(otu_mat)
+#4.Choosing the colors for variables.
 mi_color=c(“yellow”, "deepskyblue", "deeppink", “green”, “red”,”orange”)
 S <- specnumber(transp_otu_mat)
 raremax <- min(rowSums(transp_otu_mat))
@@ -13,6 +19,7 @@ otu<-rarecurve(transp_otu_mat, step = 200, sample = raremax, col=mi_color, cex =
 Nmax <- sapply(out, function(x) max(attr(x, "Subsample")))
 Smax <- sapply(out, max)
 png("rarefaction_locator.png", width = 7*300, height = 5*300, res = 400, pointsize = 8)
+
 plot(c(1, max(Nmax)), c(1, max(Smax)), xlab = "Sample Size", ylab = "Species", type = "n", cex.lab=0.8, cex.axis=0.8)
 
 #abline(v = raremax)
